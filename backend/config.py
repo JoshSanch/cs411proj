abde419e00e27b4035de36f2b87abcf019f3b861
Charles Xu <cyx2@illinois.edu>
2021-04-06 20:54:50 -0500

    Adding config.py

---
+++ backend/config.py
@@ -0,0 +1,10 @@
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = environ.get("dbURI")
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
