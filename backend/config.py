dbb7b634bdaf6b32f37e0d2b404cb6953194b261
Josh Sanchez <joshuaesanch@gmail.com>
2021-04-10 17:58:14 -0500

    Update env var name

--- backend/config.py
+++ backend/config.py
@@ -5,6 +5,6 @@ basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = environ.get("dbURI")
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URI")
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
