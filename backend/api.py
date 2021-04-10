9ccb585e32284fc125b2526187d764a6222879ac
Josh Sanchez <joshuaesanch@gmail.com>
2021-04-10 17:58:06 -0500

    Update imports and finish advanced query fn

--- backend/api.py
+++ backend/api.py
@@ -1,18 +1,20 @@
-from flask import Flask
-from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
-from sqlalchemy import inspect
+from flask import Flask, make_response, jsonify
+from flask_sqlalchemy import SQLAlchemy
 import time
+import config
 
 app = Flask(__name__)
+app.config.from_object("config.Config")
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
+    # Test that root routing is returning data correctly
     headers = {"Content-Type": "application/json"}
     return make_response(
         'Hi there',
         200,
-        headers=headers
     )
 
 @app.route('/advanced/<query_name>')
@@ -30,7 +32,8 @@ def exec_advanced_query(query_name):
                         ORDER BY games_lost DESC\
                         ) as t2\
                         WHERE games_won + games_lost > 50\
-                        ORDER BY win_rate DESC;',
+                        ORDER BY win_rate DESC\
+                        LIMIT 50;',
         "stageusage": 'SELECT stage_name, COUNT(game_id) as games_played FROM Stages NATURAL JOIN Games GROUP BY stage_name ORDER BY games_played DESC;',
         "charusage": 'SELECT char_name, games_won + games_lost AS total_games, games_won / (games_won + games_lost) AS win_rate FROM ( SELECT char_name, count(game_id) as games_won FROM Games JOIN Characters ON Games.winner_char_id = Characters.char_id GROUP BY char_name ORDER BY games_won DESC ) AS t1 NATURAL JOIN ( SELECT char_name, count(game_id) as games_lost FROM Games JOIN Characters ON Games.loser_char_id = Characters.char_id GROUP BY char_name ORDER BY games_lost DESC) AS t2 ORDER BY win_rate DESC',
         "bestcharstages": 'SELECT char_name, stage_name, count(game_id) as games_won FROM Stages NATURAL JOIN Games JOIN Characters ON Games.winner_char_id = Characters.char_id WHERE stage_name <> "" GROUP BY stage_name, char_name HAVING games_won > 3 ORDER BY char_name, games_won DESC'
@@ -38,8 +41,6 @@ def exec_advanced_query(query_name):
 
     query_str = query_to_prep[query_name]
     result = db.engine.execute(query_str)
-    return result
-
-    
-
+    return jsonify({'result': [dict(row) for row in result]})
         
+       
