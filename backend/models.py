72b414740593c7ee0da7dedd3c416503625a7017
Joshua Sanchez <joshuaesanch@gmail.com>
2021-04-12 02:20:43 -0500

    Add to_dict for models and fix some issues

--- backend/models.py
+++ backend/models.py
@@ -16,19 +16,37 @@ db = SQLAlchemy(app)
 
 
 class Players(db.Model):
+    __tablename__ = "Players"
+
     player_id = db.Column(db.String(30), unique=True, primary_key=True)
     player_name = db.Column(db.String(30))
     
     def __repr__(self):
         return '<Name: {}>'.format(self.player_name)
 
+    def to_dict(self):
+        return {
+            "player_id": self.player_id,
+            "player_name": self.player_name
+        }
+
 
 class Stages(db.Model):
+    __tablename__ = "Stages"
+
     stage_id = db.Column(db.Integer, unique=True, primary_key=True)
     stage_name = db.Column(db.String(30))
 
+    def to_dict(self):
+        return {
+            "stage_id": self.stage_id,
+            "stage_name": self.stage_name
+        }
+
 
 class Games(db.Model):
+    __tablename__ = "Games"
+
     game_id = db.Column(db.Integer, unique=True, primary_key=True)
     winner_id = db.Column(db.String(30))
     loser_id = db.Column(db.String(30))
@@ -39,13 +57,43 @@ class Games(db.Model):
     stage_id = db.Column(db.Integer)
     set_id = db.Column(db.String(30))
 
+    def to_dict(self):
+        return {
+            "game_id": self.game_id,
+            "winner_id": self.winner_id,
+            "loser_id": self.loser_id,
+            "winner_score": self.winner_score,
+            "loser_score": self.loser_score,
+            "winner_char_id": self.winner_char_id,
+            "loser_char_id": self.loser_char_id,
+            "stage_id": self.stage_id,
+            "set_id": self.set_id
+        }
+
 
 class Characters(db.Model):
+    __tablename__ = "Characters"
+
     char_id = db.Column(db.Integer, unique=True, primary_key=True)
     char_name = db.Column(db.String(30))
 
+    def to_dict(self):
+        return {
+            "char_id": self.char_id,
+            "char_name": self.char_name
+        }
+
 
 class Sets(db.Model):
+    __tablename__ = "Sets"
+
     set_id = db.Column(db.String(30), unique=True, primary_key=True)
     set_winner_id = db.Column(db.String(30))
     set_loser_id = db.Column(db.String(30))
+
+    def to_dict(self):
+        return {
+            "set_id": self.set_id,
+            "set_winner_id": self.set_winner_id,
+            "set_loser_id": self.set_loser_id
+        }
